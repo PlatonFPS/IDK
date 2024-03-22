8169e7925f3ad53ba4f3a35f4f0fa2026035a850
PlatonFPS <PlatonFPS@yandex.ru>
2024-03-22 22:34:06 +0300

    wolk animation added

--- idk/Assets/dorms/player/scripts/sc_DormsMovement.cs
+++ idk/Assets/dorms/player/scripts/sc_DormsMovement.cs
@@ -8,16 +8,16 @@ public class sc_DormsMovement : MonoBehaviour
     [SerializeField] Transform model;
 
     private CharacterController characterController;
+    private Animator animator;
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    private void SetAnimation(string animationName)
+    private void SetAnimation(string animationName, bool value)
     {
-        /*Animator animator = GetComponent<Animator>();
-        animator.Play(animationName);*/
-        //Debug.Log(animationName);
+        animator.SetBool(animationName, value);
     }
 
     [SerializeField] float movementSpeed;
@@ -35,12 +35,12 @@ public class sc_DormsMovement : MonoBehaviour
         {
             float speed = movementSpeed * Time.deltaTime;
             characterController.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed);
-            SetAnimation("Walk");
+            SetAnimation("Walk", true);
             RotatePlayer(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         }
         else 
         { 
-            SetAnimation("Idle"); 
+            SetAnimation("Walk", false); 
         }
     }
     // Update is called once per frame
