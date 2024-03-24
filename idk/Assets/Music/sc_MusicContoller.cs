54cc42d10f7ef6a1c94185568bd481e7f872a6b9
PlatonFPS <PlatonFPS@yandex.ru>
2024-03-24 19:15:21 +0300

    added fading in and out for music

--- idk/Assets/Music/sc_MusicContoller.cs
+++ idk/Assets/Music/sc_MusicContoller.cs
@@ -16,16 +16,59 @@ public class sc_MusicContoller : MonoBehaviour
     }
 
     [SerializeField] AudioClip audioClip;
+    [SerializeField] float pickUpTime;
+    [SerializeField] float dropOffTime;
+    private bool started = false;
     public void Play()
     {
         audioSource.clip = audioClip;
         audioSource.Play();
         audioSource.loop = true;
+
+        started = true;
+        Timer = pickUpTime;
     }
+    private bool ending = false;
     public void Stop()
     {
-        audioSource.Stop();
-        audioSource.loop = false;
-        audioSource.clip = null;
+        ending = true;
+        Timer = dropOffTime;
+    }
+
+    private float Timer = 0f;
+    private void Update()
+    {
+        if (started)
+        {
+            if(Timer > 0)
+            {
+                Timer -= Time.deltaTime;
+                audioSource.volume = (1 - Timer / pickUpTime) * PlayerPrefs.GetFloat("volume");
+            }
+            else
+            {
+                started = false;
+                Timer = 0f;
+                audioSource.volume = PlayerPrefs.GetFloat("volume");
+            }
+        }
+        if (ending)
+        {
+            if (Timer > 0)
+            {
+                Timer -= Time.deltaTime;
+                audioSource.volume = Timer / dropOffTime * PlayerPrefs.GetFloat("volume");
+            }
+            else
+            {
+                ending = false;
+                Timer = 0f;
+                audioSource.volume = PlayerPrefs.GetFloat("volume");
+
+                audioSource.Stop();
+                audioSource.loop = false;
+                audioSource.clip = null;
+            }
+        }
     }
 }
